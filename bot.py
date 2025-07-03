c0286496e1b22e052cbe24f72bd60101eae497fd
Bloodlinebots <githubsbots@gmail.com>
2025-07-03 20:12:17 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -371,4 +371,5 @@ if __name__ == "__main__":
         await app.updater.start_polling()
         await app.updater.idle()
 
-    asyncio.run(main())
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main())
